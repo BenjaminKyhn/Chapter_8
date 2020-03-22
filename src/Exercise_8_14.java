fbbfb3e6afebe95e7c4857231735a7fe8e05e65d
Benjamin Kyhn <benkyhn@hotmail.com>
2020-03-22 17:30:52 +0100

    Exercise 8.14 now checks the major diagonal

--- src/Exercise_8_14.java
+++ src/Exercise_8_14.java
@@ -13,8 +13,8 @@ public class Exercise_8_14 {
         int[][] identicalRows = checkForIdenticalRows(matrix);
         boolean identicals = false;
         for (int row = 0; row < matrix.length; row++) {
-            if (identicalRows[row][0] == 1){
-                System.out.println("All " + identicalRows[row][1] + "'s on row " + (row+1));
+            if (identicalRows[row][0] == 1) {
+                System.out.println("All " + identicalRows[row][1] + "'s on row " + (row + 1));
                 identicals = true;
             }
         }
@@ -25,24 +25,34 @@ public class Exercise_8_14 {
         int[][] identicalColumns = checkForIdenticalColumns(matrix);
         identicals = false;
         for (int row = 0; row < matrix.length; row++) {
-            if (identicalColumns[row][0] == 1){
-                System.out.println("All " + identicalColumns[row][1] + "'s on column " + (row+1));
+            if (identicalColumns[row][0] == 1) {
+                System.out.println("All " + identicalColumns[row][1] + "'s on column " + (row + 1));
                 identicals = true;
             }
         }
         if (!identicals)
             System.out.println("No same numbers on a column");
+
+        // Check if the identicalColumns contains identical columns and print it out if it does
+        int[] identicalDiagonal = checkForIdenticalDiagonal(matrix);
+        identicals = false;
+        if (identicalDiagonal[0] == 1) {
+            System.out.println("All " + identicalDiagonal[1] + "'s the major diagonal");
+            identicals = true;
+        }
+        if (!identicals)
+            System.out.println("No same numbers on the major diagonal");
     }
 
-    public static void fillMatrix(int[][] matrix){
+    public static void fillMatrix(int[][] matrix) {
         for (int row = 0; row < matrix.length; row++) {
             for (int column = 0; column < matrix[row].length; column++) {
-                matrix[row][column] = (int)(Math.random()*2);
+                matrix[row][column] = (int) (Math.random() * 2);
             }
         }
     }
 
-    public static void printMatrix(int[][] matrix){
+    public static void printMatrix(int[][] matrix) {
         for (int row = 0; row < matrix.length; row++) {
             for (int column = 0; column < matrix[row].length; column++) {
                 System.out.print(matrix[row][column] + " ");
@@ -53,15 +63,14 @@ public class Exercise_8_14 {
 
     // Returns a two-dimensional array where the first column indicates if the row is identical (1 = true, 0 = false)
     // and the second column indicates if the row contains 1 or 0
-    public static int[][] checkForIdenticalRows(int[][] matrix){
+    public static int[][] checkForIdenticalRows(int[][] matrix) {
         int[][] identicalsArray = new int[matrix.length][2];
         for (int row = 0; row < matrix.length; row++) {
             for (int column = 1; column < matrix[row].length; column++) {
-                if (matrix[row][column] == matrix[row][column-1]){
+                if (matrix[row][column] == matrix[row][column - 1]) {
                     identicalsArray[row][0] = 1;
                     identicalsArray[row][1] = matrix[row][column];
-                }
-                else{
+                } else {
                     identicalsArray[row][0] = 0;
                     break;
                 }
@@ -70,19 +79,34 @@ public class Exercise_8_14 {
         return identicalsArray;
     }
 
-    public static int[][] checkForIdenticalColumns(int[][] matrix){
+    // Repeat for columns
+    public static int[][] checkForIdenticalColumns(int[][] matrix) {
         int[][] identicalsArray = new int[matrix.length][2];
         for (int column = 0; column < matrix.length; column++) {
             for (int row = 1; row < matrix[column].length; row++) {
-                if (matrix[row][column] == matrix[row-1][column]){
+                if (matrix[row][column] == matrix[row - 1][column]) {
                     identicalsArray[column][0] = 1;
                     identicalsArray[column][1] = matrix[row][column];
-                }
-                else{
+                } else {
                     identicalsArray[column][0] = 0;
                     break;
                 }
             }
+        }
+        return identicalsArray;
+    }
+
+    public static int[] checkForIdenticalDiagonal(int[][] matrix) {
+        int[] identicalsArray = new int[2];
+        for (int i = 1; i < matrix.length; i++) {
+            if (matrix[i][i] == matrix[i - 1][i - 1]) {
+                identicalsArray[0] = 1;
+                identicalsArray[1] = matrix[i][i];
+            } else {
+                identicalsArray[0] = 0;
+                break;
+            }
+
         }
         return identicalsArray;
     }

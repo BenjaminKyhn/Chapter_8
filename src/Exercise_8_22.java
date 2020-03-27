dcff8bced5edc5e8e08480f3d06029b44594a905
Benjamin Kyhn <benkyhn@hotmail.com>
2020-03-27 14:01:49 +0100

    Added exercise 8.22

--- src/Exercise_8_22.java
+++ src/Exercise_8_22.java
@@ -5,6 +5,18 @@ public class Exercise_8_22 {
         int[][] matrix = new int[6][6];
         fillMatrix(matrix);
         printMatrix(matrix);
+
+        // Check rows
+        for (int row = 0; row < matrix.length; row++) {
+            if (checkRows(matrix, row))
+                System.out.println("Row " + row + " has an even number of 1's");
+        }
+
+        // Check columns
+        for (int column = 0; column < matrix[0].length; column++) {
+            if (checkColumns(matrix, column))
+                System.out.println("Column " + column + " has an even number of 1's");
+        }
     }
 
     // Fill the matrix with 0's and 1's
@@ -26,5 +38,27 @@ public class Exercise_8_22 {
         }
     }
 
+    // Method for checking rows
+    public static boolean checkRows(int[][] matrix, int row) {
+        int count = 0;
+        for (int column = 0; column < matrix[row].length; column++) {
+            if (matrix[row][column] == 1)
+                count++;
+        }
+        if (count != 0 && count % 2 == 0)
+            return true;
+        return false;
+    }
 
+    // Method for checking columns
+    public static boolean checkColumns(int[][] matrix, int column) {
+        int count = 0;
+        for (int row = 0; row < matrix.length; row++) {
+            if (matrix[row][column] == 1)
+                count++;
+        }
+        if (count != 0 && count % 2 == 0)
+            return true;
+        return false;
+    }
 }
